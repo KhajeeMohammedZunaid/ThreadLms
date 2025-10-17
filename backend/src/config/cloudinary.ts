4a21a93953ffa7c2266f27aeb0b2c35f46059983
KhajeeMohammedZunaid <23102a010452@mbu.asia>
2025-10-18 03:47:45 +0530

    new change

--- backend/src/config/cloudinary.ts
+++ backend/src/config/cloudinary.ts
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 // Get environment variables
-const cloudName = process.env.CLOUDINARY_CLOUD_NAME;
-const apiKey = process.env.CLOUDINARY_API_KEY;
-const apiSecret = process.env.CLOUDINARY_API_SECRET;
+const cloudName = process.env.CLOUDINARY_CLOUD_NAME || 'CLOUDINARY_CLOUD_NAME';
+const apiKey = process.env.CLOUDINARY_API_KEY || 'CLOUDINARY_API_KEY';
+const apiSecret = process.env.CLOUDINARY_API_SECRET || 'CLOUDINARY_API_SECRET';
 
 // Validate environment variables before configuring
 if (!cloudName || !apiKey || !apiSecret) {
